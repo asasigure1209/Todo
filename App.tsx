b3f230c03a3d99d2f57bed5a786b3c7eb014c63e
asasigure <mkmokymh@gmail.com>
2019-11-24 01:40:31 +0900

    fix: インデント統一

--- App.tsx
+++ App.tsx
@@ -1,42 +1,43 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, StatusBar, ScrollView, Image } from 'react-native';
+import { StyleSheet, View, StatusBar} from 'react-native';
 import Header from './components/Header';
 import { color } from './style/setting';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 export default function App() {
-  const [todoNames, setTodos] = useState([
-    "机を拭く",
-    "洗濯物干し",
-    "課題をする"
-  ])
+    const [todoNames, setTodos] = useState([
+        "机を拭く",
+        "洗濯物干し",
+        "課題をする"
+    ])
 
-  const completeTodo = (deleteIndex: number) => {
-    setTodos(todoNames.filter((_todo, index) => index !== deleteIndex))
-  }
+    const completeTodo = (deleteIndex: number) => {
+        setTodos(todoNames.filter((_todo, index) => index !== deleteIndex))
+    }
 
-  const cleateTodo = (title: string) => {
-    setTodos(todoNames.concat(title))
-  }
+    const cleateTodo = (title: string) => {
+    if (title !== "")
+        setTodos(todoNames.concat(title))
+    }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.statusBar}></View>
-      <Header></Header>
-      <TodoList todoNames={todoNames} onClick={completeTodo}></TodoList>
-      <TodoInput onClick={cleateTodo}></TodoInput>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.statusBar}></View>
+            <Header></Header>
+            <TodoList todoNames={todoNames} onClick={completeTodo}></TodoList>
+            <TodoInput onClick={cleateTodo}></TodoInput>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: color.background
-  },
-  statusBar: {
-    height: StatusBar.currentHeight,
-    color: '#FFFFFF'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: color.background
+    },
+    statusBar: {
+        height: StatusBar.currentHeight,
+        color: '#FFFFFF'
+    }
 });
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -9,43 +9,43 @@ export type TodoProps = {
 }
 
 export default function Todo(props: TodoProps) {
-  return (
-    <TouchableHighlight onPress={()=>{}} underlayColor={color.todo.underlay}>
-        <View style={styles.container}>
-            <Text style={styles.text} numberOfLines={1} ellipsizeMode={"tail"}>{props.title}</Text>
-            <View style={styles.imageWrapper} >
-              <TouchableHighlight onPress={() => props.onClick(props.index)} underlayColor={color.todo.background} activeOpacity={0.4}>
-                <Image style={styles.image} source={require('./../assets/check.png')} />
-              </TouchableHighlight>
+    return (
+        <TouchableHighlight onPress={()=>{}} underlayColor={color.todo.underlay}>
+            <View style={styles.container}>
+                <Text style={styles.text} numberOfLines={1} ellipsizeMode={"tail"}>{props.title}</Text>
+                <View style={styles.imageWrapper} >
+                <TouchableHighlight onPress={() => props.onClick(props.index)} underlayColor={color.todo.background} activeOpacity={0.4}>
+                    <Image style={styles.image} source={require('./../assets/check.png')} />
+                </TouchableHighlight>
+                </View>
             </View>
-        </View>
-    </TouchableHighlight>
-  );
+        </TouchableHighlight>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    backgroundColor: color.todo.background,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: color.todo.border,
-    flex: 1
-  },
-  text: {
-      fontWeight: text.fontWeight,
-      fontSize: text.fontSize,
-      color: color.todo.text,
-      textAlignVertical: 'center',
-      flex: 1
-  },
-  imageWrapper: {
-    paddingLeft: 8
-  },
-  image: {
-    width: text.fontSize + 10,
-    height: text.fontSize + 10,
-  }
+    container: {
+        padding: 16,
+        backgroundColor: color.todo.background,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: color.todo.border,
+        flex: 1
+    },
+    text: {
+        fontWeight: text.fontWeight,
+        fontSize: text.fontSize,
+        color: color.todo.text,
+        textAlignVertical: 'center',
+        flex: 1
+    },
+    imageWrapper: {
+        paddingLeft: 8
+    },
+    image: {
+        width: text.fontSize + 10,
+        height: text.fontSize + 10,
+    }
 });
